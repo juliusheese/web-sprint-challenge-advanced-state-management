63014eccb4ea3eaf593404dafb1bd606e016e0b9
Julius Shapiro <jkurt@Juliuss-MacBook-Pro.local>
2021-01-10 19:52:20 -0600

    commit cause its been a while

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,7 +1,32 @@
-import React from 'react';
+
+import { connect } from 'react-redux';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+const mapStateToProps = state => {
+    return {
+      name: state.name,
+      position: state.position,
+      nickname: state.nickname,
+      description: state.description
+    };
+  };
+  const [formData, setFormData] = useState({
+    name: '',
+    position: '',
+    nickname: '',
+    description: ''
+});
 
 class AddForm extends React.Component {
 
+   
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+     
+     
     render() {
         return(<section>
             <h2>Add Smurf</h2>
@@ -10,15 +35,25 @@ class AddForm extends React.Component {
                     <label htmlFor="name">Name:</label><br/>
                     <input onChange={this.handleChange} name="name" id="name" />
                 </div>
+                <div className="form-group">
+                    <label htmlFor="position">Position:</label><br/>
+                    <input onChange={this.handleChange} position="position" id="position" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Description:</label><br/>
+                    <input onChange={this.handleChange} description="description" id="description" />
+                </div>
+                
 
                 <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: </div>
                 <button>Submit Smurf</button>
             </form>
         </section>);
     }
+    
 }
 
-export default AddForm;
+export default connect( mapStateToProps, {AddForm})(AddForm);;
 
 //Task List:
 //1. Add in all necessary import components and library methods.
